244a07f7918f132a0574ea996c294557b6c20c44
dreis0 <migueldreis01@gmail.com>
2019-11-18 21:50:59 -0300

    Projetos adicionados

---
+++ Web/Pages/Index.cshtml.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace banco_de_dados.Pages
+{
+    public class IndexModel : PageModel
+    {
+        public void OnGet()
+        {
+
+        }
+    }
+}
