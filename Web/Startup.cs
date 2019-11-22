89c61964c28e34fa064912348c3d2f602757aa02
Miguel <migueldreis01@gmail.com>
2019-11-22 15:22:56 -0300

    Conexão com o banco e DI

--- Web/Startup.cs
+++ Web/Startup.cs
@@ -1,14 +1,11 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
-using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Npgsql;
+using System.Data;
 
 namespace banco_de_dados
 {
@@ -33,6 +30,17 @@ namespace banco_de_dados
 
 
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+
+            string con = Configuration.GetValue<string>("Connection");
+            services.AddScoped<IDbConnection>(factory => new NpgsqlConnection(con));
+
+            services.Scan(scan => scan.FromApplicationDependencies()
+                .AddClasses(classes => classes.Where(type => type.Name.Equals("QueryHandler")))
+                    .AsSelf()
+                    .WithTransientLifetime()
+                .AddClasses(classes => classes.Where(type => type.Name.Equals("CommandHandler")))
+                    .AsSelf()
+                    .WithTransientLifetime());
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
