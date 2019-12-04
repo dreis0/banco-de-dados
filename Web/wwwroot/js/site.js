b35bc63b89af1f7627446bcd34d79e59b3d8612d
Miguel <migueldreis01@gmail.com>
2019-12-04 16:51:36 -0300

    Mudanças no css

--- Web/wwwroot/js/site.js
+++ Web/wwwroot/js/site.js
@@ -1,4 +1,14 @@
-﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
-// for details on configuring this project to bundle and minify static web assets.
+﻿var options = {
+    onKeyPress: function (telefone, e, field, options) {
+        var masks = ['(00) 0000-0000', '(00) 00000-0000'];
+        var mask = (parseInt(telefone.replace(/\D/g, '')[2]) === 9) ? masks[1] : masks[0];
+        $('#Inscricao_Telefone01').mask(mask, options);
+        $('#Inscricao_Telefone02').mask(mask, options);
+    }
+};
 
-// Write your Javascript code.
+$('#Inscricao_Telefone01').mask('(00) 0000-0000', options);
+$('#Inscricao_Telefone02').mask('(00) 0000-0000', options);
+
+
+
---
+++ Web/wwwroot/js/validation.js
@@ -0,0 +1,58 @@
+﻿$(function () {
+    jQuery.validator.addMethod('validateCPF',
+        function (value, element, params) {
+
+            var sum = 0;
+            var remainder;
+            var CPF = String(value.replace(/\D/g, ''));
+
+
+            if (CPF.length !== 11)
+                return false;
+
+            var temp = '00000000000';
+            for (i = 0; i < 9; i++) {
+                if (CPF === temp)
+                    return false;
+                temp = String(parseInt(temp) + 11111111111);
+            }
+
+            for (i = 1; i <= 9; i++) {
+                var digit = parseInt(CPF.substring(i - 1, i));
+                sum += digit * (11 - i);
+            }
+
+            remainder = (sum * 10) % 11;
+
+            if (remainder === 10 || remainder === 11)
+                remainder = 0;
+
+            if (remainder !== parseInt(CPF.substring(9, 10)))
+                return false;
+
+            sum = 0;
+
+            for (i = 1; i <= 10; i++) {
+                var digit = parseInt(CPF.substring(i - 1, i));
+                sum += digit * (12 - i);
+            }
+
+            remainder = (sum * 10) % 11;
+
+            if (remainder === 10 || remainder === 11)
+                remainder = 0;
+
+            if (remainder !== parseInt(CPF.substring(10, 11)))
+                return false;
+            else
+                return true;
+        });
+
+    jQuery.validator.unobtrusive.adapters.add('validateCPF',
+        [],
+        function (options) {
+            var element = $(options.form).find('input#Inscricao_Cpf');
+            options.rules['validateCPF'] = true;
+            options.messages['validateCPF'] = options.message;
+        });
+}(jQuery));
