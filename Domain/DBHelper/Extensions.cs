97c9f29d7d088210b6c94b23cead7865daaeaf2d
Miguel <migueldreis01@gmail.com>
2019-12-04 16:13:33 -0300

    Table name helper

---
+++ Domain/DBHelper/Extensions.cs
@@ -0,0 +1,16 @@
+﻿using Domain.DBHelper;
+using System.Linq;
+
+namespace Domain
+{
+    public static class Extensions
+    {
+        public static string GetTableName<T>(this T entity)
+        {
+            var atts = entity.GetType().GetCustomAttributes(true);
+            var tableNameAtt = atts.SingleOrDefault(a => a.GetType() == typeof(TableNameAttribute));
+
+            return ((TableNameAttribute)tableNameAtt).TableName;
+        }
+    }
+}
---
+++ Domain/DBHelper/TableNameAttribute.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Domain.DBHelper
+{
+    public class TableNameAttribute : Attribute
+    {
+        public TableNameAttribute(string tableName)
+        {
+            TableName = tableName;
+        }
+
+        public string TableName { get; private set; }
+    }
+}
