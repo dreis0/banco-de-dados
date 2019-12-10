2ee99d84bc82eea442bc48cd0388ba94d0f258cd
Miguel <migueldreis01@gmail.com>
2019-12-10 14:55:52 -0300

    Enderecos do Cliente

---
+++ Domain/Cliente/EnderecoCliente.cs
@@ -0,0 +1,24 @@
+﻿using Domain.DBHelper;
+
+namespace Domain
+{
+    [TableName("EnderecoCliente")]
+    public class EnderecoCliente
+    {
+        public int Id { get; set; }
+
+        public string CpfCliente { get; set; }
+
+        public int CidadeId { get; set; }
+
+        public string Rua { get; set; }
+
+        public string Cep { get; set; }
+
+        public string Complemento { get; set; }
+
+        public int Numero { get; set; }
+
+        public bool Selecionado { get; set; }
+    }
+}
