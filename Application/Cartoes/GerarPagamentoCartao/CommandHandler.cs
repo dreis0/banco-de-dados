18b0667af5942934d7bfd96ce192943ab5771f80
Miguel <migueldreis01@gmail.com>
2019-12-11 10:43:53 -0300

    Command para pagar com cartao

---
+++ Application/Cartoes/GerarPagamentoCartao/CommandHandler.cs
@@ -0,0 +1,62 @@
+﻿using Application.Pedidos;
+using Dapper;
+using Domain;
+using System.Data;
+
+namespace Application.Cartoes
+{
+    public partial class GerarPagamentoCartao
+    {
+        public class CommandHandler
+        {
+            private readonly IDbConnection connection;
+            private readonly ObterCartao.QueryHandler obterCartaoHandler;
+            private readonly ObterPrecoTotalDoPedido.QueryHandler precoHandler;
+
+            public CommandHandler(IDbConnection db, ObterCartao.QueryHandler cartaoHandler
+                , ObterPrecoTotalDoPedido.QueryHandler preco)
+            {
+                connection = db;
+                obterCartaoHandler = cartaoHandler;
+                precoHandler = preco;
+            }
+
+            public void Handle(string cpf, int pedidoId, IDbTransaction transaction = null)
+            {
+                bool disposeTransaction = false;
+
+                if (connection.State == ConnectionState.Closed)
+                    connection.Open();
+
+                if (transaction == null)
+                {
+                    transaction = connection.BeginTransaction();
+                    disposeTransaction = true;
+                }
+
+                var cartao = obterCartaoHandler.Handle(cpf);
+                decimal preco = (decimal)precoHandler.Handle(pedidoId);
+
+                string sql = $"insert into {new PagamentoCartao().GetTableName()} " +
+                            $" (PedidoId, CartaoId, Valor) " +
+                            $" values (@PedidoId, @CartaoId, @Valor);";
+
+                connection.Execute(sql, param: new
+                {
+                    PedidoId = pedidoId,
+                    CartaoId = cartao.Id,
+                    Valor = preco
+                }, transaction: transaction);
+
+
+                if (disposeTransaction)
+                {
+                    transaction.Dispose();
+                    transaction.Commit();
+
+                    connection.Close();
+                }
+            }
+        }
+    }
+}
---
+++ Application/Cartoes/ObterCartao/QueryHandler.cs
@@ -0,0 +1,40 @@
+﻿using Dapper;
+using Domain;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Text;
+
+namespace Application.Cartoes
+{
+    public partial class ObterCartao
+    {
+
+        public class QueryHandler
+        {
+            private readonly IDbConnection connection;
+
+            public QueryHandler(IDbConnection db)
+            {
+                connection = db;
+            }
+
+            public Cartao Handle(string cpf)
+            {
+                string sql = @"select 
+                                Id
+                                ,CpfCliente
+                                ,NomeDoTitular
+                                ,Numero
+                                ,Codigo
+                                ,BandeiraId
+                                ,TipoId
+                                ,Selecionado " +
+                            $"from {new Cartao().GetTableName()} " +
+                            "where CpfCliente = @CpfCliente and selecionado";
+
+                return connection.QuerySingleOrDefault<Cartao>(sql, param: new { CpfCliente = cpf });
+            }
+        }
+    }
+}
