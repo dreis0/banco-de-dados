3602e6e93c8102297256fa28921af865ec28a56a
Miguel <migueldreis01@gmail.com>
2019-12-11 10:43:19 -0300

    Domain pagamento cartao

---
+++ Domain/Cartao/PagamentoCartao.cs
@@ -0,0 +1,14 @@
+﻿using Domain.DBHelper;
+
+namespace Domain
+{
+    [TableName("PagamentoCartao")]
+    public class PagamentoCartao
+    {
+        public int CaratoId { get; set; }
+
+        public int PedidoId { get; set; }
+
+        public double Valor { get; set; }
+    }
+}
