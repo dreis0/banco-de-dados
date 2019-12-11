ccf7f078ea4712df59caa8df040065ad20ad0f89
Miguel <migueldreis01@gmail.com>
2019-12-11 15:38:24 -0300

    Login do cliente na homepage

--- Web/Pages/ClienteViewModel.cs
+++ Web/Pages/ClienteViewModel.cs
@@ -4,7 +4,7 @@ using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
-namespace Web.Pages.Cliente
+namespace Web.Pages
 {
     public class ClienteViewModel
     {
--- Web/Pages/Index.cshtml.cs
+++ Web/Pages/Index.cshtml.cs
@@ -1,17 +1,36 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+﻿using Application.Clientes;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
-namespace banco_de_dados.Pages
+namespace Web.Pages
 {
     public class IndexModel : PageModel
     {
+        [BindProperty]
+        public ClienteViewModel Cliente { get; set; }
+
         public void OnGet()
         {
+            Cliente = new ClienteViewModel();
+        }
 
+        public IActionResult OnPost([FromServices]ObterCliente.QueryHandler queryHandler)
+        {
+            if (ModelState.IsValid)
+            {
+                var clienteResult = queryHandler.Handle(Cliente.Cpf);
+
+                if (clienteResult != null)
+                {
+                    HttpContext.Session.SetString("CpfCliente", clienteResult.Cpf);
+                    return Redirect("Cliente/Area");
+                }
+                else
+                    ModelState.AddModelError("validation", "Cpf não encontrado");
+            }
+
+            return Page();
         }
     }
 }
